3b5e4c1ec88cc5af10a03fa71118545ca74258d9
fares002 <fares@example.com>
2023-09-10 10:46:15 -0700

    k

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+printf("123456789\n");
+return (0);
+}
