d2fa72bfe128ce6270ae63377013d72528bb6c97
fares002 <fares@example.com>
2023-09-12 10:52:19 -0700

    l

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ * description:a programme that print _putchar steatment
+ * return 0
+ */
+char  main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
