4e35d7fb176b0ab9186cf5f06d527510e3659caf
fares002 <fares@example.com>
2023-09-16 13:17:11 -0700

    l

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include<stdio.h>
 /**
-*print_numbers - function that print all numbers from
+*print_most_numbers - function that print all numbers from
 *0 to 9 exepet 2 and 4
 *
 *Return: returns nothing
 */
+
 void print_most_numbers(void)
 {
 	int i;
