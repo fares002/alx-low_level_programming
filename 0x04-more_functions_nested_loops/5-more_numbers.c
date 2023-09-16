26e421ce7ae1c0f53664288cdfc620ada5fb7a38
fares002 <fares@example.com>
2023-09-16 13:44:26 -0700

    l

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
+
+void more_numbers(void)
+{
+	int row, num, count;
+
+	for (row = 0; row <= 10; row++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (num > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48)
+		}
+		_putchar('\n');
+	}
+}
