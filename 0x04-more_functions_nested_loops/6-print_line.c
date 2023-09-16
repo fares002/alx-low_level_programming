042e8987d86dbcaf2626738481f62fecda8604c3
fares002 <fares@example.com>
2023-09-16 13:56:52 -0700

    l

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+
+void print_line(int n);
+{
+	int n;
+
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		_putchar('_')
+	}
+}
