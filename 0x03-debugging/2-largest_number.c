0a152e178fa3157412643e7656e4ea84c4534a67
fares002 <fares@example.com>
2023-09-15 20:20:49 -0700

    l

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,13 +9,13 @@
  */
 
 int largest_number(int a, int b, int c)
-{int largest;
-
-if (a > b && a > c)
+{
+int largest;
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > c && b > a)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
@@ -23,6 +23,5 @@ else
 {
 largest = c;
 }
-
 return (largest);
 }
