d7eea8005c9bb6aa35c3e894e1fb0d1806c6ba48
fares002 <fares@example.com>
2023-09-16 13:03:48 -0700

    l

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9 exepet 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i >= 48; i <= 57; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
