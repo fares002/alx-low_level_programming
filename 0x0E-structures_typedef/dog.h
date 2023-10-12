d2690de78671806c7eba5f07601f62412fbce623
fares002 <fares@example.com>
2023-10-11 18:51:18 -0700

    k

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - a dog struct 
- * @name : dog's name 
+ * struct dog - a dog struct
+ * @name : dog's name
  * @age : dog's age
  * @owner : dog's owner
- * description : just long dog struct 
+ * Descrption : dog's struct that contain his name , age and his owner name
  */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
-
 #endif
