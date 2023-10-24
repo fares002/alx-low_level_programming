0a891e3451ac1653edfa8266941722385223c319
fares002 <fares@example.com>
2023-10-24 11:41:42 -0700

    fares

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,8 @@
 /**
  * print_listint - function thats return the number of node.
  * @h: pointer to the list.
- * @i: the numbers of the nodes
- * Return : the number of the nodes in the list .
+ *
+ * Return: the number of the nodes in the list.
  */
 size_t print_listint(const listint_t *h)
 {
