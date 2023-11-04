ee00d12694c8ce736607e9606f74e703cdbf88b8
fares002 <fares@example.com>
2023-11-03 18:21:00 -0700

    f

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,8 @@
 #include<fcntl.h>
 #include<stdlib.h>
 #include<unistd.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
 #define READ_BUF_SIZE 1024
 
 int create_file(const char *filename, char *text_content);
